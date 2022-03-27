bb191f6953f7d53144d91d2e48ec45cb2a8b4a68
AoufiAbderahmane <aoufi.abderahmane@lastingdynamics.com>
2022-03-27 11:06:26 +0100

    fix problem

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const image = document.getElementById("myImage");
 button.addEventListener("click", test);
 
 function test() {
-    if (image.style.display === "n   one") {
+    if (image.style.display === "none") {
         image.style.display = "block";
     } else {
         image.style.display = "none";
